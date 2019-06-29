dead5c5dfaa56c0b1de5076c442fc0c840551418
Cyrille Nofficial <cynoffic@cyrilix.fr>
2019-06-29 17:31:01 +0200

    First build

---
+++ build.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+IMG_NAME=cyrilix/k8s-sidecar
+VERSION=0.0.16
+MAJOR_VERSION=0.0
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+set -e
+set -x
+
+init_qemu() {
+    local qemu_url='https://github.com/multiarch/qemu-user-static/releases/download/v2.9.1-1'
+
+    docker run --rm --privileged multiarch/qemu-user-static:register --reset
+
+    for target_arch in aarch64 arm x86_64; do
+        wget -N "${qemu_url}/x86_64_qemu-${target_arch}-static.tar.gz";
+        tar -xvf "x86_64_qemu-${target_arch}-static.tar.gz";
+    done
+}
+
+fetch_sources() {
+    if [[ ! -d  prometheus ]] ;
+    then
+        git clone https://github.com/kiwigrid/k8s-sidecar
+    fi
+    cd k8s-sidecar
+    git checkout ${VERSION}
+}
+
+build_and_push_images() {
+    local arch="$1"
+    local dockerfile="$2"
+
+    docker build --file "${dockerfile}" --tag "${IMG_NAME}:${arch}-latest" .
+    docker tag "${IMG_NAME}:${arch}-latest" "${IMG_NAME}:${arch}-${VERSION}"
+    docker tag "${IMG_NAME}:${arch}-latest" "${IMG_NAME}:${arch}-${MAJOR_VERSION}"
+    docker push "${IMG_NAME}:${arch}-latest"
+    docker push "${IMG_NAME}:${arch}-${VERSION}"
+    docker push "${IMG_NAME}:${arch}-${MAJOR_VERSION}"
+}
+
+
+build_manifests() {
+    docker -D manifest create "${IMG_NAME}:${VERSION}" "${IMG_NAME}:amd64-${VERSION}" "${IMG_NAME}:arm-${VERSION}" --amend
+    docker -D manifest annotate "${IMG_NAME}:${VERSION}" "${IMG_NAME}:arm-${VERSION}" --os=linux --arch=arm --variant=v7
+    docker -D manifest push "${IMG_NAME}:${VERSION}"
+
+    docker -D manifest create "${IMG_NAME}:latest" "${IMG_NAME}:amd64-latest" "${IMG_NAME}:arm-latest" --amend
+    docker -D manifest annotate "${IMG_NAME}:latest" "${IMG_NAME}:arm-latest" --os=linux --arch=arm --variant=v7
+    docker -D manifest push "${IMG_NAME}:latest"
+
+    docker -D manifest create "${IMG_NAME}:${MAJOR_VERSION}" "${IMG_NAME}:amd64-${MAJOR_VERSION}" "${IMG_NAME}:arm-${MAJOR_VERSION}" --amend
+    docker -D manifest annotate "${IMG_NAME}:${MAJOR_VERSION}" "${IMG_NAME}:arm-${MAJOR_VERSION}" --os=linux --arch=arm --variant=v7
+    docker -D manifest push "${IMG_NAME}:${MAJOR_VERSION}"
+}
+
+fetch_sources
+init_qemu
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+build_and_push_images amd64 ./Dockerfile
+
+sed "s#FROM\( \+\)python:\(.*\)#FROM\1arm32v7/python:\2\n\nCOPY qemu-arm-static /usr/bin/\n#" Dockerfile > Dockerfile.arm
+build_and_push_images arm ./Dockerfile.arm
+
+build_manifests
